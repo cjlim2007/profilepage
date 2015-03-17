e38c587560dbc39726e35e598e8c5887eb843928
cjlim2007 <cjlim2007@yahoo.com>
2015-03-16 17:40:26 -0700

    everything working

--- profilepage/settings.py
+++ profilepage/settings.py
@@ -72,12 +72,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "profilepage/templates"),
+)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 ALLOWED_HOSTS = ['*']
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
--- profilepage/urls.py
+++ profilepage/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = patterns('', url(r'^$', 'profilepage.views.home', name='home'),url(r'^/info/$', 'buildup.views.info', name='info'), url(r'^/profile/$', 'buildup.views.profile', name='profile')) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = patterns('', 
+url(r'^$', 'profilepage.views.home', name='home'),
+url(r'^info/$', 'profilepage.views.info', name='info'), 
+url(r'^profile/$', 'profilepage.views.profile', name='profile') 
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
